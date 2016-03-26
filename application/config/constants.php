80f0c5784ef0e0060505574bc2bd3c58f52d507e
JaegarSarauer <jaegars@hotmail.com>
2016-03-25 18:46:35 -0700

    Parsing XML using model

--- application/config/constants.php
+++ application/config/constants.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-defined('DATAPATH', './data/');
+define('DATAPATH', './data/');
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/views/_template.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>{pageTitle}</title>
+
+	<style type="text/css"></style>
+</head>
+<body>
+    {content}
+</body>
+</html>
