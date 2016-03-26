66577149368c4bb65d345228253489723dd5dfb9
JaegarSarauer <jaegars@hotmail.com>
2016-03-25 18:46:35 -0700

    Parsing XML using model

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends Application {
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +20,19 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//$this->load->view('tables');
+                $this->data['pagebody'] = 'tables';
+               // var_dump($this->timetable->getDays());
+               // var_dump($this->timetable->getTimeslots());
+                $this->data['table1names'] = $this->getColumnNames($this->timetable->getCourses());
+                $this->data['table1'] = $this->parseToTable($this->timetable->getCourses());
+                
+                $this->data['table2names'] = $this->getColumnNames($this->timetable->getTimeslots());
+                $this->data['table2'] = $this->parseToTable($this->timetable->getTimeslots());
+                
+                $this->data['table3names'] = $this->getColumnNames($this->timetable->getDays());
+                $this->data['table3'] = $this->parseToTable($this->timetable->getDays());
+                //$this->data['table1'] = 
+                $this->render();
 	}
 }
---
+++ application/views/tables.php
@@ -0,0 +1,14 @@
+<div id="mainDiv">
+    <table id="table1">
+        {table1names}
+        {table1}
+    </table>
+    <table id="table2">
+        {table2names}
+        {table2}
+    </table>
+    <table id="table3">
+        {table3names}
+        {table3}
+    </table>
+</div>
